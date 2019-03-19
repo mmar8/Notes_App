a9ff66fabacf9dda665d740e54c5a06b655f41eb
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-19 18:31:51 +0000

    NoteListView returns correct html for 1 note

--- src/note-list-view-model.js
+++ src/note-list-view-model.js
@@ -1,9 +1,20 @@
 (function(exports){
-  function NoteListView(noteList){
+  var listOfNotes;
 
+  function NoteListView(noteList){
+    listOfNotes = noteList;
   };
   NoteListView.prototype.view = function(){
-      return "<ul></ul>"
+
+      if (listOfNotes.getNotes().length === 0){
+        return "<ul></ul>"
+      }
+      else if(listOfNotes.getNotes().length === 1){
+          return "<ul><li><div>"+listOfNotes.getNotes()[0].getText()+"</div></li></ul>"
+        }
+      else {
+
+        }
   }
   exports.NoteListView = NoteListView;
 })(this);
--- tests/note-list-view-tests.js
+++ tests/note-list-view-tests.js
@@ -1,8 +1,18 @@
 (function(exports){
   function testThatNoteListViewReturnsTheRightHtmlforAnEmptyNoteList(){
     var noteList = new NoteList();
+
     var noteListView = new NoteListView(noteList);
     assert.isTrue(noteListView.view() === "<ul></ul>")
   }
   testThatNoteListViewReturnsTheRightHtmlforAnEmptyNoteList();
+
+  function testThatNoteListViewReturnsTheRightHtmlforANoteListThatHasOneNote(){
+    var noteList = new NoteList();
+    noteList.storeNote("A note");
+    var noteListView = new NoteListView(noteList);
+    assert.isTrue(noteListView.view() === "<ul><li><div>A note</div></li></ul>")
+  }
+  testThatNoteListViewReturnsTheRightHtmlforANoteListThatHasOneNote();
+
 })(this);
