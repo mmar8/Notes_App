9f90fbb283e274d5fe552146ce87bd8a1d02474d
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-20 15:02:53 +0000

    refactored note list view

--- src/note-list-view-model.js
+++ src/note-list-view-model.js
@@ -5,21 +5,12 @@
     listOfNotes = noteList;
   };
   NoteListView.prototype.view = function(){
-
-      if (listOfNotes.getNotes().length === 0){
-        return "<ul></ul>"
-      }
-      else if(listOfNotes.getNotes().length === 1){
-          return "<ul><li><div>"+listOfNotes.getNotes()[0].getText()+"</div></li></ul>"
-        }
-      else {
-            var htmlString = "<ul>"
-          listOfNotes.getNotes().forEach(function(note){
-            htmlString+="<li><div>"+note.getText()+"</div></li>"
-          })
-          htmlString += "</ul>"
-          return htmlString;
-        }
+      var htmlString = "<ul>"
+      listOfNotes.getNotes().forEach(function(note){
+        htmlString+="<li><div>"+note.getText()+"</div></li>"
+      })
+      htmlString += "</ul>"
+      return htmlString;
   };
   exports.NoteListView = NoteListView;
 })(this);
